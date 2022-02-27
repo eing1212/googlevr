7deada4e51bc7cd549bbd45289a4245e86c6fd37
eing1212 <Phronchanok27@gmail.com>
2022-02-27 13:00:44 +0000

    first commit

---
+++ GoogleVR1/Assets/ChangeAnimator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimator : MonoBehaviour
+{
+   public void getAnimatorDefeat()
+    {
+        this.GetComponent<Animator>().Play("Defeated");
+    }
+   public void getAnimatorWalk()
+    {
+        this.GetComponent<Animator>().Play("Standing Walk Right");
+    }
+}
